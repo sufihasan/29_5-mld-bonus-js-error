0cc2ef314e2a91fd70a33b3ad8d15439e85a2fff
sufihasan <hasan6nh@gmail.com>
2022-08-23 14:06:10 +0600

    first commit

---
+++ js/error.js
@@ -0,0 +1,39 @@
+const number = [10, 20, 30, 40];
+// console.log(number);
+// console.log(number[38]); // the 38 index is not in this array and the js error is undifined
+
+
+// // reference errror
+// console.log(hasanId);
+// let hasanId = 81;
+
+
+// // syntex error
+// for (i = 0; i < 10: i++) {
+
+//     console.log(i);
+// }
+
+
+// //type error
+// var num = 123;
+// const my = num.toUpperCase();
+// console.log(my);
+
+
+console.log('bithy'); //this line is before error 
+console.log('Hasan');//this line is before error 
+console.log(81);//this line is before error 
+console.log(20);//this line is before error 
+
+try {
+    console.log(money); //error line
+} catch (error) {  // error is user define u can use e or other words
+    console.log(error);  //this line print error  
+}
+
+
+
+console.log('Green'); //this line is after error 
+console.log('university');//this line is after error 
+
